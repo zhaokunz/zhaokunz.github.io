855b60a969b7dce49605c315ec0939371c7b2a84
zhaokun <zk1253@qq.com>
2019-09-05 23:52:39 +0800

    哈哈

---
+++ datas.js
@@ -0,0 +1,54 @@
+Vue.component('lee',{
+    template:`
+        <div id="text">
+            <table class="table" style="height:370px;">
+                <tr style="height:40px;text-align:center;">
+                    <td v-for="(item, index) in arr[0]">
+                        <a :href="item.url">{{item.name}}</a>
+                    </td>
+                </tr>
+
+
+
+
+                <tr></tr>
+            </table>
+        </div>
+    `,
+    data(){
+        return{
+            arr:[
+                [
+                    {
+                        name:'9-4轮播图',
+                        url:'Look/9-4/轮播图.html',
+                    },
+                    {
+                        name:'9-4三级导航',
+                        url:'Look/9-4/三级导航.html',
+                    },
+                ]
+            ]
+        }
+    }
+})
+new Vue({
+    el: '#box',
+    data:{
+        drawer: false,
+        direction: 'ttb',
+    },
+    methods: {
+        add() {
+            this.drawer = true;
+        },
+        handleClose(done) {
+            console.log(3)
+            this.$confirm('确认关闭？')
+                .then(_ => {
+                    done();
+                })
+                .catch(_ => {});
+        }
+    }
+})
